469dbf912cb2ba5154633c1b54a03c76193e1055
Beatriz França <beatriz.rbfranca@hotmail.com>
2025-02-22 15:20:40 -0300

    fix: timeZone

--- BotecoPoker.Mvc/Global.asax.cs
+++ BotecoPoker.Mvc/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -11,6 +12,13 @@ namespace BotecoPoker.Mvc
             var culture = new System.Globalization.CultureInfo("pt-BR");
             System.Threading.Thread.CurrentThread.CurrentCulture = culture;
             System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+
+            TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            DateTime now = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone);
+
+            System.Threading.Thread.CurrentThread.CurrentCulture.DateTimeFormat = culture.DateTimeFormat;
+            System.Threading.Thread.CurrentThread.CurrentUICulture.DateTimeFormat = culture.DateTimeFormat;
+
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
